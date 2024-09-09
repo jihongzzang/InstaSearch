704cb0649c68b485632f0ba0553c6ce13e482bc7
jujihong <jujihong2@gmail.com>
2024-09-10 01:26:36 +0900

    Initial Commit

---
+++ InstaSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaSearch
+//
+//  Created by 주지홍 on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
